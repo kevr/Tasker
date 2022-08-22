a3dfee00ef2e6fb326d05248cb247ac6d281b8d7
Kevin Morris <kevr@0cost.org>
2022-08-22 00:57:23 -0700

    feat: add googletest, create first test
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ src/main.hpp
@@ -0,0 +1,6 @@
+#ifndef SRC_MAIN_HPP
+#define SRC_MAIN_HPP
+
+const char PROGNAME[] = "tasker";
+
+#endif /* SRC_MAIN_HPP */
---
+++ src/tests/main.test.cpp
@@ -0,0 +1,14 @@
+#define main main_real
+#include "main.cpp"
+#undef main
+
+#include <gtest/gtest.h>
+
+TEST(main, runs)
+{
+    const char *_argv[] = { PROGNAME, nullptr };
+    auto argv = const_cast<char **>(_argv);
+
+    auto rc = main_real(1, argv);
+    ASSERT_EQ(rc, 0);
+}
