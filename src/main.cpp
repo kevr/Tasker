b77d32dcd8c9833d9d8050973f5b661a7fd10b90
Kevin Morris <kevr@0cost.org>
2022-08-21 23:17:44 -0700

    feat: add meson project and main.cpp
    In addition, added a .gitignore file to keep things clean
    in a local tree.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Description: Main program entry-point.
+ *
+ * Copyright (C) 2022 Kevin Morris <kevr@0cost.org>
+ * All Rights Reserved.
+ **/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
