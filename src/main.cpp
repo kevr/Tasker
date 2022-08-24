414df0635f66087307bf1033c30d9a3b4404c2b2
Kevin Morris <kevr@0cost.org>
2022-08-23 23:05:38 -0700

    fix: error check ncurses calls
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,25 @@
  **/
 #include "config.hpp"
 #include "ncurses.hpp"
+#include "utility.hpp"
 #include <iostream>
+
+int tasker_main(tasker::ext::ncurses &ncurses, int argc, char *argv[])
+{
+    std::cout << PROG << " " << VERSION << std::endl;
+    if (!ncurses.initscr()) {
+        return error(1, "ncurses.initscr() failed");
+    }
+
+    if (auto rc = ncurses.keypad(ncurses.root(), true)) {
+        return error(2, "ncurses.keypad failed: ", rc);
+    }
+
+    return ncurses.endwin();
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << PROG << " " << VERSION << std::endl;
-
     auto ncurses = tasker::ext::ncurses();
-    ncurses.initscr();
-    ncurses.keypad(ncurses.root(), true);
-    return ncurses.endwin();
+    return tasker_main(ncurses, argc, argv);
 }
--- src/tests/main.test.cpp
+++ src/tests/main.test.cpp
@@ -2,8 +2,38 @@
 #include "main.cpp"
 #undef main
 
-#include "../ncurses.hpp"
+#include "../mocks/ncurses.hpp"
 #include <gtest/gtest.h>
+
+using ::testing::_;
+using ::testing::Return;
+
+TEST(main, initscr_fails)
+{
+    tasker::ext::mock_ncurses ncurses;
+    EXPECT_CALL(ncurses, initscr()).WillOnce(Return(nullptr));
+
+    const char *_argv[] = { PROG, nullptr };
+    auto argv = const_cast<char **>(_argv);
+
+    auto rc = tasker_main(ncurses, 1, argv);
+    ASSERT_EQ(rc, 1);
+}
+
+TEST(main, keypad_fails)
+{
+    tasker::ext::mock_ncurses ncurses;
+
+    WINDOW win;
+    EXPECT_CALL(ncurses, initscr()).WillOnce(Return(&win));
+    EXPECT_CALL(ncurses, keypad(_, _)).WillOnce(Return(ERR));
+
+    const char *_argv[] = { PROG, nullptr };
+    auto argv = const_cast<char **>(_argv);
+
+    auto rc = tasker_main(ncurses, 1, argv);
+    ASSERT_EQ(rc, 2);
+}
 
 TEST(main, runs)
 {
